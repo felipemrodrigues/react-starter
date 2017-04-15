105484026397d269883c79bbf0732f2dc23ea53c
Felipe Rodrigues <feliperodrigues@gmail.com>
2017-04-15 12:45:31 -0300

    Adding initial test file

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
         <div>Teste</div>
     );
   }
 }
+
+export default App;
